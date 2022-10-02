c0f0ae9e97b8e7ffd4417d9c40f22b6aa02720f0
MasterKelwin <mkelwin@outlook.com>
2022-10-02 09:59:44 -0300

    adding HttpClient

--- src/services/quotes.service.ts
+++ src/services/quotes.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuotesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
 }
